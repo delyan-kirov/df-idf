a9ad053e796ecc12abe996dd9b8cdf1b0f466ecd
delyan-kirov <delyan.k.kirov@gmail.com>
2024-05-01 23:51:28 +0300

    Adding sql query and speed up for data insertion

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use rusqlite::{params, Connection, Result};
 use std::collections::HashMap;
 use std::fs;
 use std::path::{Path, PathBuf};
+use std::sync::atomic::AtomicUsize;
 use std::sync::{Arc, Mutex};
 
 type TermHash = HashMap<String, usize>;
@@ -26,6 +27,21 @@ fn create_document_table(document: &Document) -> Result<()> {
         // Escape codes in sqlite table names
         .replace("/", "_IN_")
         .replace(".", "_DOT_");
+    // Create the table if it doesn't exist
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS documents (
+                name TEXT PRIMARY KEY,
+                size INTEGER NOT NULL
+            )",
+        [],
+    )?;
+    // add document to sql
+    conn.execute(
+        "INSERT OR REPLACE INTO documents 
+            (name, size) VALUES 
+            (?1  , ?2)",
+        [&table_name, &document.df.iter().count().to_string()],
+    )?;
 
     // Create the table if it doesn't exist
     conn.execute(
@@ -39,31 +55,42 @@ fn create_document_table(document: &Document) -> Result<()> {
         [],
     )?;
     // Populating the database with values from the HashMap
+    let mut sql_insert = "".to_string();
     for (term, frequency) in &document.df {
-        conn.execute(
-            &format!(
-                "INSERT OR REPLACE INTO {} 
+        sql_insert.push_str(&format!(
+            "INSERT OR REPLACE INTO {} 
                     (term, frequency) VALUES 
-                    (?1  , ?2)",
-                table_name
-            ),
-            params![term, frequency],
-        )?;
+                    ('{}'  , {});\n",
+            &table_name, &term, &frequency
+        ));
     }
+    conn.execute(&sql_insert, params![])?;
     return Ok(());
 }
 
 fn add_terms_to_sqlite(terms: &TermHash) -> Result<()> {
     // Create db connection
     let conn = Connection::open(DATABASE_PATH)?;
+    // Create the table if it doesn't exist
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS terms (
+                term TEXT PRIMARY KEY,
+                frequency INTEGER NOT NULL
+            )",
+        [],
+    )
+    .expect("ERROR: could not create the table for terms");
+    // Create db connection
+    let mut sql_insert = "".to_string();
     for (term, frequency) in terms {
-        conn.execute(
+        sql_insert.push_str(&format!(
             "INSERT OR REPLACE INTO terms 
                     (term, frequency) VALUES 
-                    (?1  , ?2)",
-            params![term, frequency],
-        )?;
+                    ('{}'  , {});\n",
+            &term, &frequency
+        ));
     }
+    conn.execute(&sql_insert, params![])?;
     Ok(())
 }
 
@@ -82,8 +109,46 @@ fn process_file(path: &Path) -> Option<Document> {
     })
 }
 
+fn query_term(term: &String) -> Result<String> {
+    let conn = Connection::open(DATABASE_PATH)?;
+    #[derive(Debug, Clone)]
+    struct RowData {
+        name: String,
+        size: usize,
+    }
+    let mut doc_names = Vec::new();
+    let mut doc_sizes = Vec::new();
+    conn.prepare(&format!(
+        "SELECT name, size FROM documents WHERE name = {}",
+        &term
+    ))?
+    .query_map(&[term], |row| {
+        let name = row.get(0)?;
+        let size = row.get(1)?;
+        Ok(RowData { name, size })
+    })?
+    .into_iter()
+    .for_each(|row| {
+        let curr_row = row.expect("ERROR: could not unwrap doc_names");
+        doc_names.push(curr_row.name);
+        doc_sizes.push(curr_row.size);
+    });
+    let query: String = doc_names.iter().fold("".to_string(), |name_1, name_2| {
+        format!(
+            "SELECT frequency FROM {} WHERE term = {}
+             UNION
+             SELECT frequency FROM {} WHERE term = {}",
+            &name_1, &term, &name_2, &term
+        )
+    });
+    dbg!(&query);
+    Ok(query)
+    // Ok("".to_string())
+}
+
 fn main() {
     let dir_path = Path::new("./content");
+
     let total_terms: Arc<Mutex<TermHash>> = Arc::new(Mutex::new(HashMap::new()));
     let mut files: Vec<PathBuf> = Vec::new();
     if let Ok(entries) = fs::read_dir(dir_path) {
@@ -91,27 +156,18 @@ fn main() {
             if let Ok(entry) = entry {
                 let path = entry.path();
                 if path.is_file() {
-                    println!("INFO: file: {:?} was added", &path);
+                    // println!("INFO: file: {:?} was added", &path);
                     files.push(path.clone());
                 }
             }
         }
     }
-    // Create db connection
-    let conn = Connection::open(DATABASE_PATH)
-        .expect("ERROR: could not open a connection to the database");
-    // Create the table if it doesn't exist
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS terms (
-                term TEXT PRIMARY KEY,
-                frequency INTEGER NOT NULL
-            )",
-        [],
-    )
-    .expect("ERROR: could not create the table for terms");
+
+    let counter = AtomicUsize::new(0); // Initialize atomic counter
+    let unsuccessful_files = Arc::new(Mutex::new(Vec::new()));
 
     let _ = files
-        .iter()
+        .par_iter_mut()
         .filter_map(|path| process_file(path))
         .for_each(|doc| {
             let mut total_terms = total_terms.lock().expect("ERROR: Could not lock mutex");
@@ -121,21 +177,42 @@ fn main() {
             drop(total_terms); // Release the lock
             match create_document_table(&doc) {
                 Ok(_) => {
-                    println!("INFO: Successfully indexed document {:?}", &doc.path);
+                    let count = counter.fetch_add(1, std::sync::atomic::Ordering::Relaxed) + 1;
+                    print!("\rINFO: Successfully indexed document {}", count);
                 }
-                Err(e) => {
-                    println!("ERROR: {}", e);
+                Err(_) => {
+                    let mut unsuccessful_files = unsuccessful_files
+                        .lock()
+                        .expect("ERROR: Could not lock mutex");
+                    unsuccessful_files.push(doc.path);
                 }
             };
         });
+    println!("");
     if let Err(e) = add_terms_to_sqlite(&total_terms.lock().expect("ERROR: could not lock mutex")) {
         println!("ERROR: {}", e);
     };
+    let unsuccessful_files = unsuccessful_files
+        .lock()
+        .expect("ERROR: could not lock mutex for unsuccessful_files");
+
+    if !unsuccessful_files.is_empty() {
+        println!("ERROR: the following files could not be indexed: ",);
+        for file in unsuccessful_files.iter() {
+            println!(
+                "  - {}",
+                &file
+                    .to_path_buf()
+                    .to_str()
+                    .expect("ERROR: could not unwrap PathBuf")
+            );
+        }
+    }
+    println!("{:?}", query_term(&"of".to_string()));
 }
 
 /*
  *
  * TODO: Given a query of terms, make the relavant sql query
- * TODO: Try posgress
  *
  * */
